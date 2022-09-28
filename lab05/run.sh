d42788712727cdb6b31bd5ae41e02ae503ef8db1
SlavSquatSuperstar <ericweng03@gmail.com>
2022-09-27 21:17:23 -0400

    Completed Lab 5 exercises

---
+++ lab05/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run SimBase.java from lab04/ with lab05/fib.binary
+cd ../lab04
+javac SimBase.java
+java SimBase ../lab05/fib.binary
+# About 2,000 steps are required to execute the entire program
